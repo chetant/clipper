854c853642e5aa6743692328fae23f7b01066635
Chetan T <chetant@gmail.com>
2011-08-08 07:54:24 -0400

    Upload-ready release

--- test/Test.hs
+++ test/Test.hs
@@ -3,7 +3,7 @@ import Algebra.Clipper
 main = do
   let p1 = Polygon [IntPoint 0 0, IntPoint 100 0, IntPoint 100 100, IntPoint 0 100]
       p2 = Polygon [IntPoint 0 0, IntPoint 100 0, IntPoint 50 200]
-  soln <- execute ctIntersection (Polygons [p1]) (Polygons [p2])
+  soln <- (Polygons [p1]) <@> (Polygons [p2])
   area <- polygonArea p1
   clk <- polygonIsClockwise p1
   putStrLn $ "Intersection:" ++ show soln
